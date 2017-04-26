36be89e2e95f17c62ff5e74995eebf0da70124b0
mrcmrcmrc <miraccaylak@gmail.com>
2017-04-26 19:39:50 +0300

    Update lastFM.py

--- lastFM.py
+++ lastFM.py
@@ -12,103 +12,165 @@ lovedTracksURL = "https://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks"
 topTracksURL = "https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks"
 similarTracksURL = "https://ws.audioscrobbler.com/2.0/?method=track.getsimilar"
 geoTopTracksURL = "https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks"
+artistTopTracksURL = "https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks"
+tagTopTracksURL = "https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks"
+
+
 
 def getLovedTracks(username):
+    result = []
     page = 1
-    artists = []
-    tracks = []
     nextPage = True
+    
     while nextPage:
         URL = lovedTracksURL + "&user=" + username + "&api_key=" + API_KEY + "&page=" + str(page) + "&format=json"
         try:
             response = urllib.urlopen(URL)
             data = json.loads(response.read())
         except:
-            return False
+            return None
 
         if 'error' in data:
             print data['message']
-            return False
+            return None
         else:
             for t in data['lovedtracks']['track']:
-                artists.append(t['artist']['name'])
-                tracks.append(t['name'])
+                temp_list = []
+                temp_list.append(t['artist']['name'])
+                temp_list.append(t['name'])
+
+                result.append(temp_list)
+
             totalPages = data['lovedtracks']['@attr']['totalPages']
             if int(totalPages) > page:
                 page += 1
             else:
                 nextPage = False
-    return artists, tracks
+    return result
+
+
 
 def getTopTracks(username, period, limit):
     #periods: overall | 7day | 1month | 3month | 6month | 12month
     URL = topTracksURL + "&user=" + username + "&period=" + period + "&limit=" + str(limit) + "&api_key=" + API_KEY + "&format=json"
-    artists = []
-    tracks = []
+    result = []
     try:
         response = urllib.urlopen(URL)
         data = json.loads(response.read())
     except:
-        return False
+        return None
 
     if 'error' in data:
         print data['message']
-        return False
+        return None
     else:
         for t in data['toptracks']['track']:
-            artists.append(t['artist']['name'])
-            tracks.append(t['name'])
-    return artists, tracks
+            temp_list = []
+            temp_list.append(t['artist']['name'])
+            temp_list.append(t['name'])
+
+            result.append(temp_list)
+    
+    return result
+
+
 
 def getSimilar(artist, track, limit):
-    artists = []
-    tracks = []
+    result = []
     URL = similarTracksURL + "&artist=" + artist + "&track=" + track + "&api_key=" + API_KEY + "&limit=" + str(
         limit) + "&autocorrect=1" + "&format=json"
+    
     try:
         response = urllib.urlopen(URL)
         data = json.loads(response.read())
     except:
-        return False
+        return None
 
     if 'error' in data:
         print data['message']
-        return False
+        return None
     else:
         founded_track = len(data["similartracks"]["track"])
         for i in range(founded_track):
-            artists.append(data["similartracks"]["track"][i]["artist"]["name"])
-            tracks.append(data["similartracks"]["track"][i]["name"])
+            temp_list = []
+            temp_list.append(data["similartracks"]["track"][i]["artist"]["name"])
+            temp_list.append(data["similartracks"]["track"][i]["name"])
 
-    return artists, tracks
+            result.append(temp_list)
+
+    return result
 
 
 
 def getGeoTopTracks (country, limit):
-    artists = []
-    tracks = []
+    result = []
+    URL = geoTopTracksURL + "&country=" + country + "&api_key=" + API_KEY + "&limit=" + str(limit) +  "&format=json"
+    
+    try:
+        response = urllib.urlopen(URL)
+        data = json.loads(response.read())
+    except:
+        return None
 
-    if limit % 50 == 0:
-        totalPages = limit / 50
+    if 'error' in data:
+        print data['message']
+        return None
     else:
-        totalPages = (limit / 50) + 1
+        for t in data['tracks']['track']:
+            temp_list = []
+            temp_list.append(t['artist']['name'])
+            temp_list.append(t['name'])
 
-    total = 0
-    for page in range(1, totalPages + 1):
-        URL = geoTopTracksURL + "&country=" + country + "&api_key=" + API_KEY + "&page=" + str(page) + "&limit=" +  "&format=json"
-        try:
-            response = urllib.urlopen(URL)
-            data = json.loads(response.read())
-        except:
-            return False
+            result.append(temp_list)
 
-        if 'error' in data:
-            print data['message']
-            return False
-        else:
-            for t in data['tracks']['track']:
-                artists.append(t['artist']['name'])
-                tracks.append(t['name'])
-                total = total + 1
-                if total == limit:
-                    return artists, tracks
+    return result
+
+
+
+def getArtistTopTracks (artist, limit):
+    result = []
+    URL = artistTopTracksURL + "&artist=" + artist + "&api_key=" + API_KEY + "&limit=" + str(limit) + "&format=json"
+        
+    try:
+        response = urllib.urlopen(URL)
+        data = json.loads(response.read())
+    except:
+        return None
+
+    if 'error' in data:
+        print data['message']
+        return None
+    else:
+        for t in data['toptracks']['track']:
+            temp_list = []
+            temp_list.append(artist)
+            temp_list.append(t['name'])
+
+            result.append(temp_list)
+
+    return result
+
+
+
+def getTopTracksForTag (tag, limit):
+    result = []
+    URL = tagTopTracksURL + "&tag=" + tag + "&limit=" + str(limit) +"&api_key=" + API_KEY + "&format=json"
+    
+    try:
+        response = urllib.urlopen(URL)
+        data = json.loads(response.read())
+    except:
+        return None
+
+    if 'error' in data:
+        print data['message']
+        return None
+    else:
+        for t in data['tracks']['track']:
+            temp_list = []
+            temp_list.append(t['artist']['name'])
+            temp_list.append(t['name'])
+
+            result.append(temp_list)
+
+    return result
